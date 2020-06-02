0466ffeebfff528ada331dbfa84a9f1c53cf5eb1
anandjos <anandjose5@gmail.com>
2020-06-03 00:03:41 +0530

    Remove comments

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isArmstrongNumber = (num) => {
 var n=num,sum=0;
 var d=num.toString().length;
