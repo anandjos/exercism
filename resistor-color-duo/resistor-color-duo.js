9862b0d960b27d4d0ddc9d3470b7fbc18e8044a7
anandjos <anandjose5@gmail.com>
2020-06-05 22:47:55 +0530

    Use helper function

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,10 @@
-export const decodedValue = (colorcode) => {
-  const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-  let index1 = COLORS.indexOf(colorcode[0]);
-  let index2 = COLORS.indexOf(colorcode[1]);
-  if(index1 == -1 || index2 == -1)
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+const indexfun = (color) => {
+  let index = COLORS.indexOf(color);
+  if(index == -1)
     throw new RangeError("Invalid input colour");
-  return index1*10 + index2;  
+  return index;
+ };
+export const decodedValue = (colorcode) => {
+  return indexfun(colorcode[0])*10 + indexfun(colorcode[1]); 
 };
