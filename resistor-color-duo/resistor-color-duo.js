55a9b8153b39f03771f5f2ef3a43cae9a6e93917
anandjos <anandjose5@gmail.com>
2020-06-06 02:30:54 +0530

    Use template literals

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,10 +1,6 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-const indexfun = (color) => {
-  let index = COLORS.indexOf(color);
-  if(index == -1)
-    throw new RangeError("Invalid input colour");
-  return index;
- };
 export const decodedValue = (colorcode) => {
-  return indexfun(colorcode[0])*10 + indexfun(colorcode[1]); 
+  let index1=COLORS.indexOf(colorcode[0]);
+  let index2=COLORS.indexOf(colorcode[1]);
+  return Number(`${index1}${index2}`);
 };
