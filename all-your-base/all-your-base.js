ddb62c69a6157aaf65707d30e9e2e1cb53936a9e
anandjos <anandjose5@gmail.com>
2020-06-08 11:18:06 +0530

    Remove unnecessary lines

--- all-your-base/all-your-base.js
+++ all-your-base/all-your-base.js
@@ -9,24 +9,20 @@ export const convert = (num,a=-1,b=-1) => {
   if( l > 1 && num[0] == 0 )
     throw new Error("Input has wrong format");
   let num_b = [];
-  let dec = 0,i = 0,valid = 0;
+  let i,dec = 0;
   for(i = 0; i < l; i++)
   {
-    valid = 1;
     let powval = Math.pow(a,l-i-1);
     if( num[i] >= a || num[i] < 0 )
       throw new Error("Input has wrong format");
     dec = dec + num[i]*powval;
   }
-  if(dec == 0 && valid == 1)
-  {
-    num_b = num_b.concat(dec);
-    return num_b;
-  }  
   while(dec!=0)
   {
     num_b.unshift(dec%b);
     dec = Math.floor(dec/b);
-  }
+  } 
+  if( num[0] == 0 )
+    num_b = num_b.concat(0);
   return num_b;
 };
