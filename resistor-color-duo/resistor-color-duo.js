b36bb2dd70b13935985843d67a88b4c24e50206e
anandjos <anandjose5@gmail.com>
2020-06-08 18:50:48 +0530

    Remove variables

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,4 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];  
 export const decodedValue = (colorcode) => {
-  let index1=COLORS.indexOf(colorcode[0]);
-  let index2=COLORS.indexOf(colorcode[1]);
-  return Number(`${index1}` + `${index2}`);
+  return Number(`${COLORS.indexOf(colorcode[0])}` + `${COLORS.indexOf(colorcode[1])}`);
 };
