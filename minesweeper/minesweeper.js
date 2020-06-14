b149f73ad3a8ca2909559a2efb0cca6d0a4daf1f
anandjos <anandjose5@gmail.com>
2020-06-14 19:13:32 +0530

    Update adjacent_mine_count in minesweeper

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,20 +1,8 @@
-const find_num = (input,i,j) => {
-  let r = input.length;
-  let c = input[0].length;
-  let x1=0,y1=0,x2=r-1,y2=c-1;
+const adjacent_mine_count = (input,i,j) => {
   let count = 0;
-  if(i > 0)
-    x1 = i-1;
-  if( i < r-1)
-    x2 =i+1;
-  if(j > 0)
-    y1 = j-1;
-  if(j < c-1)
-    y2 = j+1;
-  for(let i = x1; i <= x2; i++)
-    for(let j = y1; j <= y2; j++)
-      if(input[i][j] == '*')
-        count++;
+  for(let k=i-1; k<i+2; k++)
+    if(input[k])
+    count = count + input[k].substring(j-1,j+2).split('*').length -1;
   if(count == 0)
     return ' ';
   return count;
@@ -35,7 +23,7 @@ export const annotate = (input) =>  {
       if(input[i][j] == '*')
         output[i] = output[i].concat('*');
       else
-        output[i] = output[i].concat(find_num(input,i,j));
+        output[i] = output[i].concat(adjacent_mine_count(input,i,j));
   }
   return output;
 }
