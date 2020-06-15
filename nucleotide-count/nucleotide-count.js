69ba9b204fc4adf330b3fe8fa0e8915eb6a5b7dd
anandjos <anandjose5@gmail.com>
2020-06-15 16:07:08 +0530

    Remove comments

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -2,8 +2,6 @@ export class NucleotideCounts {
   static parse(dna) {
     let count = '';
     let nucleotide_count = { A: 0,C: 0,G: 0,T: 0 };
-   // let count_obj = Object.create(nucleotide_count);
-   // console.log(count_obj.hasOwnProperty('A'));
     dna.split('').forEach(function(nucleotide){
       if(!nucleotide_count.hasOwnProperty(nucleotide))
         throw new RangeError("Invalid nucleotide in strand");
