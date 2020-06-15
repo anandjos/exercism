f44f4d50075b2c9524caaaca492299ba86c05004
anandjos <anandjose5@gmail.com>
2020-06-15 19:29:25 +0530

    Use binary conversion in allergies

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -1,26 +1,19 @@
-const score = { 1: 128,2: 64, 3: 32,4: 16,5: 8,6: 4,7: 2,8: 1};
-const items = { 128: 'cats',64: 'pollen',32: 'chocolate',16: 'tomatoes',8: 'strawberries',4: 'shellfish',2: 'peanuts',1: 'eggs' }; 
+const allergens = ['eggs','peanuts','shellfish','strawberries','tomatoes','chocolate','pollen','cats'];
 export class Allergies {
   constructor(num) {
-    this.score = num;
+    this.num = num%256;
+    this.score = (this.num%256).toString(2).split('').reverse();
     this.allergic_items = [];
-    for(let [key,value] of Object.entries(score))
-    {
-      if(this.score>=value)
-      {
-        this.score =this.score%value;
-        this.allergic_items.unshift(items[value]);
-        if(this.score == 0)
-          break;
-      }
-    }
+    for(let i = 0; i<this.score.length; i++)
+      if(this.score[i] == 1)
+        this.allergic_items.push(allergens[i]);
   }
 
   list() {
     return this.allergic_items;
   }
 
-  allergicTo(item) {
-    return this.allergic_items.includes(item);
+  allergicTo(allergen) {
+    return this.allergic_items.includes(allergen);
   }
 }
