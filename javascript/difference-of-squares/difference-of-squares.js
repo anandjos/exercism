d34a40a95a039765501aa985550973638e92eeb1
anandjos <anandjose5@gmail.com>
2020-06-22 17:39:27 +0530

    Update difference-of-squares

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,22 +1,22 @@
 export class Squares {
   constructor(limit) {
     this.limit = limit;
-    this.sumofsq = 0;
-    this.sqofsum = 0;
   }
 
   get sumOfSquares() {
+    let result = 0;
     for(let i = 1; i<=this.limit; i++)
-      this.sumofsq += i*i;
-    return this.sumofsq;
+      result += i*i;
+    return result;
   }
 
   get squareOfSum() {
-      this.sqofsum = Math.pow(this.limit*(this.limit+1)/2,2);
-    return this.sqofsum;
+    let result = 0;
+      result = Math.pow(this.limit*(this.limit+1)/2,2);
+    return result;
   }
 
   get difference() {
-    return this.sqofsum-this.sumofsq;
+    return this.squareOfSum-this.sumOfSquares;
   }
 }
