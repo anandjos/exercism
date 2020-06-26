4b26769b3aefe39b649cc13c109d0d81ed46360f
anandjos <anandjose5@gmail.com>
2020-06-27 00:19:31 +0530

    Update word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,11 +1,7 @@
 export const countWords = (line) => {
   let word_count = {};
-  let regex = /([a-z]+'[a-z]+)|\w+/g;
+  let regex = /\w+('\w+)?/g;
   let words = line.toLowerCase().match(regex);
-  words.forEach(function(word){
-    if(word_count.hasOwnProperty(word))
-      word_count[word]++;
-    else word_count[word] = 1;
-  });
+  words.forEach(word => word_count[word]++ || (word_count[word] = 1));
   return word_count;
 };
