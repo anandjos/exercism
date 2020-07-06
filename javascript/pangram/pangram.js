9e2c09c00649a0bb6e1143376870b4253529e5d5
anandjos <anandjose5@gmail.com>
2020-07-06 23:45:24 +0530

    Use spread operator in pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,6 +1,4 @@
+const ALPHABETS = [...'abcdefghijklmnopqrstuvwxyz'];
 export const isPangram = (line) => {
-  let alphabets = ['a' ,'b' ,'c','d' ,'e','f' ,'g' ,'h','i' ,'j' ,'k' ,
-                   'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,
-                   'w' , 'x', 'y', 'z'];
-  return alphabets.every( alphabet => line.toLowerCase().split('').includes(alphabet));
+  return ALPHABETS.every( alphabet => line.toLowerCase().includes(alphabet));
 };
