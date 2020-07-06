f964a600ff72f2c0bf9db551f00b6968df0d53df
anandjos <anandjose5@gmail.com>
2020-07-06 21:24:28 +0530

    Update pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,13 +1,6 @@
 export const isPangram = (line) => {
-  let alphabets = {a:0 ,b:0 ,c:0 ,d:0 ,e:0 ,f:0 ,g:0 ,h:0 ,i:0 ,j:0 ,k:0 ,
-                   l:0 ,m:0 ,n:0 ,o:0 ,p:0 ,q:0 ,r:0 ,s:0 ,t:0 ,u:0 ,v:0 ,
-                   w:0 , x:0, y:0, z:0};
-  line.split('').forEach(function(letter){
-    if(alphabets.hasOwnProperty(letter.toLowerCase()))
-      alphabets[letter.toLowerCase()]++;
-  });
-  for(let [key,value] of Object.entries(alphabets))
-    if(value == 0)
-      return false;
-  return true;
+  let alphabets = ['a' ,'b' ,'c','d' ,'e','f' ,'g' ,'h','i' ,'j' ,'k' ,
+                   'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,
+                   'w' , 'x', 'y', 'z'];
+  return alphabets.every( alphabet => line.toLowerCase().split('').includes(alphabet));
 };
