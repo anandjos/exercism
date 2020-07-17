912eb4527b94e031b8ca8f310369cd083903097f
anandjos <anandjose5@gmail.com>
2020-07-18 02:24:34 +0530

    Update beer-song

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -2,16 +2,11 @@ export const recite = (initialBottlesCount, takeDownCount) => {
   let lyrics = [];
   for(let i = initialBottlesCount; i>initialBottlesCount-takeDownCount; i--)
   {
-    if(i > 1)
+    if(i >= 1)
     {
-      lyrics.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
-      if(i > 2)
-        lyrics.push(`Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
-      else lyrics.push(`Take one down and pass it around, 1 bottle of beer on the wall.`);
+      lyrics.push(`${i} bottle${i>1? 's' :''} of beer on the wall, ${i} bottle${i>1? 's' :''} of beer.`);
+      lyrics.push(`Take ${i>1?'one':'it'} down and pass it around, ${i-1>0? `${i-1}` :'no more'} bottle${i-1!=1? 's' :''} of beer on the wall.`);
     }
-    else if(i == 1)
-      lyrics.push('1 bottle of beer on the wall, 1 bottle of beer.',
-        'Take it down and pass it around, no more bottles of beer on the wall.');
     else lyrics.push('No more bottles of beer on the wall, no more bottles of beer.', 
         'Go to the store and buy some more, 99 bottles of beer on the wall.');
     lyrics.push('');
