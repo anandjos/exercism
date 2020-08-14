11b7650ed1ceca19bded3bfa658b5fa0b865ebe3
anandjos <anandjose5@gmail.com>
2020-08-14 14:11:41 +0530

    Update pascals-triangle

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,13 +1,15 @@
 export const rows = (height) => {
-  let pascals_triangle = [];;
-  for(let i = 0; i<height; i++)
+  if(height == 0)
+    return [];
+  let pascals_triangle = [[1]];
+  for(let i = 1; i<height; i++)
   {
     let row = [];
     for(let j = 0; j<=i; j++)
-      if(pascals_triangle[i-1] != undefined)
-        row.push( (pascals_triangle[i-1][j-1] + pascals_triangle[i-1][j])||1 );
-      else 
-        row.push(1);
+    {
+      let previousLeft = pascals_triangle[i-1][j-1],previousRight = pascals_triangle[i-1][j];
+      row.push( (previousLeft + previousRight)||1 );
+    }
     pascals_triangle.push(row);
   }
   return pascals_triangle;
